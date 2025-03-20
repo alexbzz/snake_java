73c8bf2269a0cd37ab68014ebae0dbc0b0ebf29e
Alexandre BOZZI <alexandre.bozzi@ynov.com>
2025-03-20 14:54:12 +0100

    feat: class Snake with all methode

--- src/entities/Snake.java
+++ src/entities/Snake.java
@@ -5,12 +5,76 @@ import java.util.LinkedList;
 
 public class Snake extends Entity {
     private LinkedList<Point> body;
-    public Snake(Point startPosition, int speed) {
+    private int length;
+    private String color;
+    private int speed;
+    private int life;
+    private boolean status; // Actif ou non
+    private String skin;
+
+    public Snake(Point startPosition, int speed, String color, String skin) {
         super(startPosition, speed, "RIGHT");
-        body = new LinkedList<>();
-        body.add(startPosition);
+        this.body = new LinkedList<>();
+        this.body.add(startPosition);
+        this.length = 1;
+        this.color = color;
+        this.speed = speed;
+        this.life = 3;
+        this.status = true;
+        this.skin = skin;
     }
-    public LinkedList<Point> getBody() { return body; }
+
+    public int getLength() { return length; }
+    public String getColor() { return color; }
+    public int getSpeed() { return speed; }
+    public int getLife() { return life; }
+    public boolean getStatus() { return status; }
+    public String getSkin() { return skin; }
+
+    public void setLength(int length) { this.length = length; }
+    public void setColor(String color) { this.color = color; }
+    public void setSpeed(int speed) { this.speed = speed; }
+    public void setLife(int life) { this.life = life; }
+    public void setStatus(boolean status) { this.status = status; }
+    public void setSkin(String skin) { this.skin = skin; }
+
+    public void grow(int values) {
+        this.length += values;
+        for (int i = 0; i < values; i++) {
+            body.add(new Point(body.getLast()));
+        }
+    }
+
+    public void reduce(int values) {
+        if (this.length > values) {
+            this.length -= values;
+            for (int i = 0; i < values; i++) {
+                body.removeLast();
+            }
+        }
+    }
+
+    public void accelerate(int values) {
+        this.speed += values;
+    }
+
+    public void slow(int values) {
+        if (this.speed > values) {
+            this.speed -= values;
+        }
+    }
+
+    public void loseLife(int values) {
+        this.life -= values;
+        if (this.life <= 0) {
+            this.status = false;
+        }
+    }
+
+    public boolean isDead() {
+        return this.life <= 0;
+    }
+
     @Override
     public void move() {
         Point newHead = new Point(position);
@@ -22,7 +86,17 @@ public class Snake extends Entity {
         }
 
         body.addFirst(newHead);
-        body.removeLast();
+        if (body.size() > length) {
+            body.removeLast();
+        }
         position = newHead;
     }
+    public void changeDirection(String up) {
+        switch (direction) {
+            case "UP" -> direction = "UP";
+            case "DOWN" -> direction = "DOWN";
+            case "LEFT" -> direction = "LEFT";
+            case "RIGHT" -> direction = "RIGHT";
+        }
     }
+}
